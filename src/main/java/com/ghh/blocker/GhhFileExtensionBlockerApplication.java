8ef0fa7b2fc6af4bfc4143df7bb6bec3ce61d94c
khha <khha@jcone.co.kr>
2026-02-09 14:44:36 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/ghh/blocker/GhhFileExtensionBlockerApplication.java
@@ -0,0 +1,13 @@
+package com.ghh.blocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhhFileExtensionBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhhFileExtensionBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghh/blocker/GhhFileExtensionBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghh.blocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhhFileExtensionBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
